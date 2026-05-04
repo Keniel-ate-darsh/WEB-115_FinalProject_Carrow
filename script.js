40c1044189a1dda45a68a4e1627d280c3dfcaa39
Keniel Carrow <kcarrow@my.waketech.edu>
2026-05-04 13:56:09 -0400

    Place Tokens

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ let numRows = 6;
 let numCols = 7;
 let circleRadius = canvas.width/(numCols*2);
 let turnRed = true;
-
+let tokenGrid = Array.from({ length: numRows }, () => Array(numCols).fill("white"));
 function start() {
     drawBoard();
-    //canvas.addEventListener("click",placeToken);
+    canvas.addEventListener("click",placeToken);
 }
 function drawBoard(){
     context.fillStyle = "blue";
@@ -18,7 +18,7 @@ for(let row=0; row <  numRows; row++){
     for(let col = 0; col < numCols; col++){
         let x = col * cellWidth + cellWidth / 2 
         let y = row * cellHeight + cellHeight / 2
-        drawCircle(x,y,circleRadius,"white")
+        drawCircle(x,y,circleRadius,tokenGrid[row][col])
 }
 }
 }
@@ -28,5 +28,22 @@ function drawCircle(x, y, radius, color) {
     context.fillStyle = color;
     context.fill();
     context.closePath();
+}
+
+function placeToken(event){
+    let rect = canvas.getBoundingClientRect();
+    let xClick = event.clientX - rect.left;
+    let col = Math.floor(xClick / (canvas.width / numCols));
+   for (let row = numRows - 1; row >= 0; row--) {
+        if (tokenGrid[row][col] === "white") {
+            tokenGrid[row][col] = turnRed ? "red" : "yellow";
+            turnRed = !turnRed;
+            break;
+    }
+}
+drawBoard();
+}
+function checkWinner(grid,token){
+
 }
 start();
